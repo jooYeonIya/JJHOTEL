5b18b29abae3b65c0174df9b6665f951d156266c
Gahyeon19 <wjsrkgus0119@naver.com>
2024-10-10 17:20:10 +0900

    design: logo + title 추가 (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
+
   return (
     <>
-    <h1>JJHotel</h1>
-    <hr />
+    <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <>
+      <img src="src\images\logo.png" alt="logo" width='20px' height='20px'/>
+      JJ HOTEL
+    </>
+  );
+}
+
+export default Header;
