9c0640c430dea6d96abc68445698f3fa6ac0b8e3
Gahyeon19 <wjsrkgus0119@naver.com>
2024-10-10 17:27:48 +0900

    design: 메뉴 4개 추가 (#3)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,25 @@
 function Header() {
   return (
     <>
-      <img src="src\images\logo.png" alt="logo" width='20px' height='20px'/>
+      <img src="src\images\logo.png" alt="logo" width="20px" height="20px" />
       JJ HOTEL
+      <br />
+      <div>
+        <ul>
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Rooms</a>
+          </li>
+          <li>
+            <a>Reservation</a>
+          </li>
+          <li>
+            <a>Facilities</a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
