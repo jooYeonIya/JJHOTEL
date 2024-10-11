eddff841f2a07dcdae702784111da18826434b53
Gahyeon19 <wjsrkgus0119@naver.com>
2024-10-11 09:28:25 +0900

    design: Footer 안에 호텔 정보 나열, 하단에 고정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
     <Header />
+    <p>CONTENTS</p>
+    <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import '../css/Footer.css'
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <hr />
+        <img src="src\images\logo.png" alt="logo" width="20px" height="20px" /> JJ HOTEL
+        <div>
+          <p>경기도 성남시 분당구 성남대로 34 </p>
+          <p>031-606-9311</p>
+        </div>
+        <hr />
+      </footer>
+    </>
+  )
+}
+
+export default Footer
