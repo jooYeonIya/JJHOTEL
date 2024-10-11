04620361bb81d64a5b7b436543d429c4169fbfa7
jooyeoniya <jooyeonstudyios@gmail.com>
2024-10-11 17:19:08 +0900

    design: 캘린더 추가 (#9)

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -1,36 +1,13 @@
-import { useState } from "react"
-import ReservationCalendar from "./ReservationCalendar"
 import TitleLabel from "./TitleLabel"
+import ReservationCalendar from "./ReservationCalendar"
 
 function Reservation() {
-
-  const getTomorrowDate = () => {
-    const today = new Date()
-    const tomorrowDate = new Date(today)
-    tomorrowDate.setDate(today.getDate() + 1)
-    return tomorrowDate
-  }
-
-  const [date, setDate] = useState([
-    new Date(),
-    getTomorrowDate(),
-  ])
-
-  const showStartDate = (date) => {
-    return date.length > 0 ? date[0].toLocaleDateString() : date.toLocaleDateString()
-  }
-
-  const showEndDate = (date) => {
-    return date.length > 0 ? date[1].toLocaleDateString() : date.toLocaleDateString()
-  }
 
   return (
     <>
       <TitleLabel title={"Reservation"} subTitle={"날짜 및 인원 선택"}></TitleLabel>
       <br />
-      <ReservationCalendar date={date} dateChange={setDate} />
-      startDate: {showStartDate(date)}
-      endDate: {showEndDate(date)}
+      <ReservationCalendar />
     </>
   )
 }
--- src/components/ReservationCalendar.jsx
+++ src/components/ReservationCalendar.jsx
@@ -1,14 +1,32 @@
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+import { useState } from 'react';
+import { DateRange } from 'react-date-range';
+import { addDays } from "date-fns"
+import { ko } from 'date-fns/locale'
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
 
-export default function ReservationCalendar({ date, dateChange }) {
+export default function ReservationCalendar() {
+
+    const [date, setDate] = useState([{
+        startDate: new Date(),
+        endDate: addDays(new Date(), 1),
+        key: 'selection'
+    }])
+
     return (
         <>
-            <Calendar
-                onChange={dateChange}
-                defaultValue={date} 
-                showNeighboringMonth={false}
-                selectRange={true}
+            <DateRange
+                editableDateInputs={true}
+                onChange={(item) => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                months={2}
+                direction="horizontal"
+                showDateDisplay={false}
+                showMonthAndYearPickers={false}
+                locale={ko} 
+                monthDisplayFormat="MM월"
+                minDate={new Date()}
             />
         </>
     )
