863bd0f44b9d3be2ec0f23ea908d1de7b0ec7d45
jooyeon <jooyeonstudyios@gmail.com>
2024-10-13 22:38:42 +0900

    design: 타이틀 추가 (#16)

--- src/components/RoomDescription.jsx
+++ src/components/RoomDescription.jsx
@@ -1,12 +1,33 @@
 import { useLocation } from "react-router-dom"
+import TitleLabel from "./TitleLabel"
+import { useEffect, useState } from "react"
 
 export default function RoomDescription() {
   const location = useLocation()
   const roomId = location.state?.roomId
+  const [room, setRoom] = useState()
+
+  useEffect(() => {
+    getRoomInfo(roomId).then(setRoom)
+  }, [roomId])
+
+  if (!room) return <p>Loading...</p>
 
   return(
     <>
-      <p>{roomId}</p>
+      <TitleLabel title={room.name} subTitle={room.description} />
     </>
   )
+}
+
+async function getRoomInfo(roomId) {
+  const mockRoom = { 
+    id: 1, 
+    name: "Deluxe", 
+    description: "도시의 전경이 어쩌구~",
+    imageURL1: "src/images/facilities_dining.jpg",
+    imageURL2: "src/images/about_information.jpg",
+    imageURL3: "src/images/facilities_pool.jpg",
+    imageURL4: "src/images/facilities_dining.jpg" }
+  return mockRoom
 }
