77a0a1f4b746ede451ecdb15576c36eb402f7a42
jooyeonIya <jooyeonstudyios@gmail.com>
2024-10-13 18:52:55 +0900

    feat: counter 기능 추가 (#9)

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,12 +3,20 @@ import { useState } from "react"
 export default function Counter({ title }) {
     const [counter, setCounter] = useState(0)
 
+    const plusCounter = () => {
+        setCounter(counter + 1)
+    }
+
+    const minusCounter = () => {
+        setCounter(counter + 1)
+    }
+
     return (
         <>
             {title}
-            <button>+</button>
+            <button onClick={plusCounter}>+</button>
             {counter}
-            <button>-</button>
+            <button onClick={minusCounter}>-</button>
         </>
     )
 }
