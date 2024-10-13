f9e4184f4efb8c60d7451f5f9e5fd5f8c5a1d853
jooyeon <jooyeonstudyios@gmail.com>
2024-10-13 22:51:43 +0900

    design: 메인 이미지 1장, 서브 이미지들 4장 추가하고 서브 이미지들 클릭했을 때 메인 이미지 변경 (#16)

--- src/components/RoomDescription.jsx
+++ src/components/RoomDescription.jsx
@@ -6,16 +6,39 @@ export default function RoomDescription() {
   const location = useLocation()
   const roomId = location.state?.roomId
   const [room, setRoom] = useState()
+  const [mainImage, setMainImage] = useState()
+  const [subImages, setSubImages] = useState([])
 
   useEffect(() => {
-    getRoomInfo(roomId).then(setRoom)
+    getRoomInfo(roomId).then((data) => {
+      setRoom(data)
+      setMainImage(data.imageURL1)
+      setSubImages([data.imageURL1, data.imageURL2, data.imageURL3, data.imageURL4])
+    })
   }, [roomId])
+
+  const changeMainImage = (imageURL) => {
+    setMainImage(imageURL)
+  }
 
   if (!room) return <p>Loading...</p>
 
   return(
     <>
       <TitleLabel title={room.name} subTitle={room.description} />
+      <div id="mainImage">
+        <img src={mainImage} alt="" width={400} height={300}/>
+      </div>
+      <div id="subImages">
+        {subImages.map((image) => 
+          <img 
+            src={image} 
+            alt="" 
+            onClick={ () => changeMainImage(image) }
+            width={100} 
+            height={100}/>
+        )}
+      </div>
     </>
   )
 }
