fe4daef9a891eafbe856960f378a59e4758b02bc
Gahyeon <wjsrkgus0119@naver.com>
2024-10-14 09:54:50 +0900

    design: Header css 추가, Home.jsx 추가 (#5)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+
+
+function Home() {
+
+  
+
+
+  return (
+    <>
+      <div>        
+        <img src="src\images\home_main.jpg" alt="home_main" width="100%" height="100%"/>
+      </div>
+      <h2>All About </h2>
+      <h1>JJ HOTEL</h1>
+    </>
+  )
+}
+
+export default Home
