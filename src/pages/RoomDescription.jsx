763032535366b19eaffaa63ddde2b70e54166470
jooyeoniya <jooyeonstudyios@gmail.com>
2024-10-16 09:33:33 +0900

    design: 객실 정보 css 추가 (#39)

--- src/pages/RoomDescription.jsx
+++ src/pages/RoomDescription.jsx
@@ -1,8 +1,10 @@
 import { useLocation } from "react-router-dom"
-import TitleLabel from "../components/TitleLabel"
 import { useEffect, useState } from "react"
+import TitleLabel from "../components/TitleLabel"
 import Header from "../components/Header"
 import axios from "axios"
+
+import "../css/RoomDescription.css"
 
 export default function RoomDescription() {
   const location = useLocation()
@@ -17,12 +19,12 @@ export default function RoomDescription() {
 
   async function getRoomInfo(roomId) {
     axios.post("http://localhost:3003/roomDescription", { roomId })
-    .then(res => {
-      let room = res.data[0]
-      setRoom(room)
-      setMainImage(room.imageURL1)
-      setSubImages([room.imageURL1,room.imageURL2, room.imageURL3, room.imageURL4])
-    })
+      .then(res => {
+        let room = res.data[0]
+        setRoom(room)
+        setMainImage(room.imageURL1)
+        setSubImages([room.imageURL1, room.imageURL2, room.imageURL3, room.imageURL4])
+      })
   }
 
   const changeMainImage = (imageURL) => {
@@ -31,43 +33,70 @@ export default function RoomDescription() {
 
   if (!room) return <p>Loading...</p>
 
-  return(
+  return (
     <>
-      <Header isEvent={true}/>
-      <TitleLabel title={room.roomName} subTitle={room.description} />
+      <Header isEvent={true} />
 
-      <div id="mainImage">
-        <img src={mainImage} alt="" width={400} height={300}/>
-      </div>
-      <div id="subImages">
-        {subImages.map((image) => 
-          <img 
-            src={image} 
-            alt="" 
-            onClick={ () => changeMainImage(image) }
-            width={100} 
-            height={100}/>
-        )}
-      </div>
-      <div id="infomaiton">
-        <p>객실 정보</p>
-        size: {room.size}m
-        Bed type: 더블 or 트윈
-        Room Features: 베드룸1, 베스룸1
-        price: {room.price}원
-        최대 수용 인원: {room.maxNumberOfPeople}명
-      </div>
-      <div id="service">
-        <p>객실 서비스</p>
-        <li>미니바</li>
-        <li>커피/티 메이커</li>
-        <li>미니 냉장고</li>
-        <li>에어컨</li>
-        <li>Wi-Fi</li>
-        <li>스마트 TV</li>
-        <li>어메니티</li>
-        <li>브러시</li>
-        <li>헤어 드라이기</li>
+      <div className="container">
+        <TitleLabel title={room.roomName} subTitle={room.description} />
+
+        <div className="mainImage">
+          <img src={mainImage} alt="" width={400} height={300} />
+        </div>
+        <div className="subImages">
+          {subImages.map((image) =>
+            <img
+              src={image}
+              alt=""
+              onClick={() => changeMainImage(image)}
+              width={100}
+              height={100} />
+          )}
+        </div>
+
+        <div className="infomaitonBox">
+          <div className="infomaitonLeft">
+            <p>객실 정보</p>
+          </div>
+
+          <div className="infomaitonRigth">
+            <ul>
+              <li>
+                <strong>size</strong>
+                <p>{room.size}m</p>
+              </li>
+              <li>
+                <strong>Bed type</strong>
+                <p>더블 or 트윈</p>
+              </li>
+              <li>
+                <strong>Room Features</strong>
+                <p>베드룸1, 베스룸1</p>
+              </li>
+              <li>
+                <strong>price</strong>
+                <p>{room.price}원</p>
+              </li>
+              <li>
+                <strong>최대 수용 인원</strong>
+                <p>{room.maxNumberOfPeople}명</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="service">
+          <p>객실 서비스</p>
+          <li>미니바</li>
+          <li>커피/티 메이커</li>
+          <li>미니 냉장고</li>
+          <li>에어컨</li>
+          <li>Wi-Fi</li>
+          <li>스마트 TV</li>
+          <li>어메니티</li>
+          <li>브러시</li>
+          <li>헤어 드라이기</li>
+        </div>
       </div>
     </>
   )
