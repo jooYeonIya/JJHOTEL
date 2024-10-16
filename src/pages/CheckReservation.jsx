d02845eb87e176262af2b06d483116bcfdb73d5e
Gahyeon19 <wjsrkgus0119@naver.com>
2024-10-16 10:30:01 +0900

    design: CheckReservation 화면의 고객 정보 컴포넌트 css 추가

--- src/pages/CheckReservation.jsx
+++ src/pages/CheckReservation.jsx
@@ -35,11 +35,20 @@ function CheckReservation() {
     <div>
       <Header isEvent={true}/>
       <div className="container">
-      <p>고객 정보</p>
-      성명 <input type="text" value={name} onChange={(e) => setName(e.target.value)} /><br />
-      예약번호 <input type="text" value={reservationId} onChange={(e) => setReservationId(e.target.value)} /><br />
-      <CustomButton title="예약 조회" onClicked={checkReservation}/>
-      {isClicked && reservationData && <ReservationInfo reservation={reservationData}/>}
+        <h2>고객 정보</h2>
+        <div className="input_container">
+          <div className="input_row">
+            <label className="input_label">성명</label>
+            <input className="input_name" type="text" value={name} onChange={(e) => setName(e.target.value)} /><br />
+          </div>
+          
+          <div className="input_row">
+            <label className="input_label">예약번호</label>
+            <input className="input_id" type="text" value={reservationId} onChange={(e) => setReservationId(e.target.value)} /><br />
+            <CustomButton title="예약 조회" onClicked={checkReservation}/>
+          </div>
+        </div>
+        {isClicked && reservationData && <ReservationInfo reservation={reservationData}/>}
       </div>
     </div>
   )
