6ffc9146d64f7bdd646d22b0bc40727a976582c0
Gahyeon19 <wjsrkgus0119@naver.com>
2024-10-16 15:07:17 +0900

    design: Home 화면의 main 이미지 수정 (#44)

--- src/components/MainImageContainer.jsx
+++ src/components/MainImageContainer.jsx
@@ -4,7 +4,7 @@ export default function MainImageContainder() {
     return (
         <>
             <div className="mainImageContainder">
-                <img src={"src/images/hotel_main.jpg"} alt="home_main" />
+                <img src={"src/images/home_main.jpg"} alt="home_main" />
                 <div className="reservationInputContainer">
                     <ReservaionInputContainer />
                 </div>
