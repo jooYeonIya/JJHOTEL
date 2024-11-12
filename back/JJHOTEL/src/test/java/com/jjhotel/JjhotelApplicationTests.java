3d22bc22fde7506a488bb2c7c976397d526b8bf8
kostackdgk <kostackdgkim@naver.com>
2024-11-12 12:20:35 +0900

    first init

---
+++ back/JJHOTEL/src/test/java/com/jjhotel/JjhotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjhotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
