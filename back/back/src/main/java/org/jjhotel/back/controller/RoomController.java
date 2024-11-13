117eed79494d961e25f617a06b8d55335644cd40
jooyeoniya <jooyeonstudyios@gmail.com>
2024-11-13 10:13:02 +0900

    feat: spring으로 이관 (#50)

--- back/back/src/main/java/org/jjhotel/back/controller/RoomController.java
+++ back/back/src/main/java/org/jjhotel/back/controller/RoomController.java
@@ -1,11 +1,20 @@
 package org.jjhotel.back.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.jjhotel.back.domain.dto.RoomListDto;
 import org.jjhotel.back.service.RoomService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/room")
 public class RoomController {
+  private final RoomService roomService;
+
+  @GetMapping("/all")
+  public List<RoomListDto> getAllRooms() {
+    return roomService.getAllRooms();
+  }
 }
