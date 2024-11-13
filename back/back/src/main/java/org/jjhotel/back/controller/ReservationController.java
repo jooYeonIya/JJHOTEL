a61329d1b6e0fac34d3cb3f4b9a6ce123db1f152
jooyeoniya <jooyeonstudyios@gmail.com>
2024-11-13 19:51:11 +0900

    feat: 예약하기 spring으로 변경 (#60)

--- back/back/src/main/java/org/jjhotel/back/controller/ReservationController.java
+++ back/back/src/main/java/org/jjhotel/back/controller/ReservationController.java
@@ -4,20 +4,29 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jjhotel.back.domain.dto.ReservationInfoDto;
 import org.jjhotel.back.domain.dto.ReservationWithGuestInfoDto;
+import org.jjhotel.back.domain.dto.RoomReservationDto;
 import org.jjhotel.back.service.ReservationService;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/reservation")
 public class ReservationController {
     private final ReservationService reservationService;
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/check")
     public ReservationInfoDto getReservationInfo(@RequestBody ReservationWithGuestInfoDto reservationWithGuestInfoDto) {
         ReservationInfoDto reservationInfo = reservationService.getReservationInfo(reservationWithGuestInfoDto);
         return reservationService.getReservationInfo(reservationWithGuestInfoDto);
     }
 
+    @PostMapping()
+    public String doReservation(@RequestBody RoomReservationDto info) {
+        // guestId 는 보안.. 작업하면서 .. 세션에서 가져오든.. 어떻게든..
+        return reservationService.doReservation(info, "asdf");
+    }
 }
