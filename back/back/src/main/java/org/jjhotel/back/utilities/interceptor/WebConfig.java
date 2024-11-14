13311d0b69ba8afd5679b9a0cb154fa8b5193a8b
jooyeoniya <jooyeonstudyios@gmail.com>
2024-11-14 16:52:20 +0900

    etc: 차후 보안 필요 화면 업데이트 할 필요 있음 (지금은 테스트를 위해 모두 개방해둔 상태) (#65)

--- back/back/src/main/java/org/jjhotel/back/utilities/interceptor/WebConfig.java
+++ back/back/src/main/java/org/jjhotel/back/utilities/interceptor/WebConfig.java
@@ -1,19 +1,21 @@
 package org.jjhotel.back.utilities.interceptor;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-  @Autowired
-  private LoginCheckInterceptor loginCheckInterceptor;
+  private final LoginCheckInterceptor loginCheckInterceptor;
+
+  public WebConfig(LoginCheckInterceptor loginCheckInterceptor) {
+    this.loginCheckInterceptor = loginCheckInterceptor;
+  }
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(loginCheckInterceptor)
-        .addPathPatterns("/**")
-        .excludePathPatterns(""); //차후 화면 리스트업해서 수정 필요
+        .addPathPatterns("")
+        .excludePathPatterns("/**"); //차후 화면 리스트업해서 수정 필요
   }
 }
